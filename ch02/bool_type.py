f77670515b8e2391fbd4dd93181b5d7c280e81e2
clin7821 <clin7812@gmail.com>
2026-07-24 14:02:37 +0800

    上傳bool_type.py

---
+++ ch02/bool_type.py
@@ -0,0 +1,7 @@
+b=False
+print(type(b))  #宣告b為布林變數，變數值為False
+print(type(1))  #顯示布林變數b的類別
+print(bool(1))  #顯示整數常值1的類別
+print(bool(-1))  #使用bool函式將1轉成布林值
+print(bool('Python'))  #使用bool函式將-1轉成布林值
+print(bool())  #使用bool函式將'Python'轉成布林值
---
+++ ch02/variable.py
@@ -0,0 +1,8 @@
+yesNo ='y'  #宣告yesNo變數，並指定變數值為字串'y'
+#同時宣告passScore、maxScore、minScore三個變數，並指定變數值為60、100和0
+passScore, maxScore, minScore = 60,100, 0
+power1 = power2 = 100  #宣告power1和power2變數，變數值都為100
+total = 1.23456e+6  #宣告total變數，並設變數值為浮點數1234560.0
+del total  #刪除total變數
+
+
